a1f6b0a62fe95e6446e0abb8eee26c9f7297e879
Code Begineer <hrandhaw@usc.edu>
2017-03-09 22:42:57 -0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -7,25 +7,24 @@
 //Given "bbbbb", the answer is "b", with the length of 1.
 
 public class Solution {
-public int lengthOfLongestSubstring(String s) {
-int max = 0, start = 0, i = 0;
-HashSet<Character> set = new HashSet<Character>();
-while(i<s.length()){
-char e = s.charAt(i);
-if(!set.contains(e)){
-set.add(e);
-}
-else{
-max = Math.max(max,set.size());
-while(start<i && s.charAt(start)!=e){
-set.remove(s.charAt(start));
-start++;
-}
-start++;
-}
-i++;
-}
-return Math.max(max,set.size());
-
-}
+  public int lengthOfLongestSubstring(String s) {
+    int max = 0, start = 0, i = 0;
+    HashSet<Character> set = new HashSet<Character>();
+    while(i<s.length()){
+      char e = s.charAt(i);
+      if(!set.contains(e)){
+        set.add(e);
+      }
+    else{
+      max = Math.max(max,set.size());
+      while(start<i && s.charAt(start)!=e){
+        set.remove(s.charAt(start));
+        start++;
+      }
+      start++;
+    } 
+    i++;
+  }
+  return Math.max(max,set.size());
+  }
 }
