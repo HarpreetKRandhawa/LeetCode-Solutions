1969a6a2fcbc4ce3b257137395dbe568e61ec629
Code Begineer <hrandhaw@usc.edu>
2017-03-09 20:30:10 -0800

    Reverse a Linked List
    Time Complexity O(n)
    Space Complexity O(n)

---
+++ Question206.java
@@ -0,0 +1,20 @@
+//Reverse a singly linked list.
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode second = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return second;
+    }
+}
