309dd8e161b342a374efdc7b245b32c731ac691f
Code Begineer <hrandhaw@usc.edu>
2017-06-01 15:01:57 -0700

    Rotate Image

--- Question48.java
+++ Question48.java
@@ -3,13 +3,16 @@
 public class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        for(int i=0;i<n/2;i++){
-            for(int j = 0; j<Math.ceil(((double) n) / 2.);j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[n-1-j][i];
-                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
-                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
-                matrix[j][n-1-i] = temp;
+        for(int layer=0; layer<n/2; layer++){
+            int first = layer;
+            int last = n-1-layer;
+            for(int i=first; i<last; i++){
+                int offset = i-first;
+                int top = matrix[first][i];
+                matrix[first][i] = matrix[last-offset][first];
+                matrix[last-offset][first] = matrix[last][last-offset];
+                matrix[last][last-offset] = matrix[i][last];
+                matrix[i][last] = top;
             }
         }
     }
