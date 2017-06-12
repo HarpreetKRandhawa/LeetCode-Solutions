1fd3f0cdae3fd22462209ee1020ccb8958a513a8
Code Begineer <hrandhaw@usc.edu>
2017-06-12 11:53:47 -0700

    Maximum Product Subarray

---
+++ Question152.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int maxProduct(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        if(nums.length == 1)
+            return nums[0];
+        int[] max = new int[nums.length];
+        int[] min = new int[nums.length];
+        max[0] = min[0] = nums[0];
+        int res = nums[0];
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] > 0){
+                max[i] = Math.max(nums[i],nums[i]*max[i-1]);
+                min[i] = Math.min(nums[i],nums[i]*min[i-1]);
+            }
+            else{
+                max[i] = Math.max(nums[i],nums[i]*min[i-1]);
+                min[i] = Math.min(nums[i],nums[i]*max[i-1]);
+            }
+            res = Math.max(res,max[i]);
+        }
+        return res;
+    }
+}
