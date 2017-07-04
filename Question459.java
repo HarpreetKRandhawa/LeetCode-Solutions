e27510a7f92dfa7ef99c2957ff8f4be4b36b32f3
Code Begineer <hrandhaw@usc.edu>
2017-07-03 20:51:12 -0700

    Repeated Substring Pattern
    Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
    
    Example 1:
    Input: "abab"
    Output: True
    Explanation: It's the substring "ab" twice.

---
+++ Question459.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int l = s.length();
+        for(int i=l/2; i>0; i--){
+            if(l % i == 0){
+                int m = l/i;
+                String sub = s.substring(0,i);
+                StringBuilder sb = new StringBuilder();
+                for(int j=0; j<m; j++){
+                    sb.append(sub);
+                }
+                if(sb.toString().equals(s))
+                    return true;
+            }
+        }
+        return false;
+    }
+}
