3cdeeeea14e81a7c7e2fa0ce3d8bf6b95d707fe2
Code Begineer <hrandhaw@usc.edu>
2017-10-28 15:42:11 -0700

    Kth Largest Element in an Array
    ind the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
    
    For example,
    Given [3,2,1,5,6,4] and k = 2, return 5.

--- Question215.java
+++ Question215.java
@@ -4,3 +4,32 @@ public class Solution {
         return nums[nums.length-k];
     }
 }
+
+//Solution 2 Time Complexity O(n)
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if(nums == null || nums.length == 0) return Integer.MAX_VALUE;
+        return findKthLargest(nums,0,nums.length-1,nums.length-k);
+    }
+    public int findKthLargest(int[] nums, int start, int end, int k){
+        if(start>end) return Integer.MAX_VALUE;
+        int left = start;
+        int pivot = nums[end];
+        for(int i=start; i<end; i++){
+            if(nums[i] <= pivot)
+                swap(nums,left++,i);
+        }
+        swap(nums,left,end);
+        if(left == k)
+            return nums[left];
+        else if(left < k)
+            return findKthLargest(nums,left+1,end,k);
+        else 
+            return findKthLargest(nums,start,left-1,k);
+    }
+     void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
