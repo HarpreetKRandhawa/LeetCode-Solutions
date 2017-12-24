2a5216253c1d372a16a73575c2515209a9148099
Code Begineer <hrandhaw@usc.edu>
2017-12-24 08:55:33 -0800

    Unique Word Abbreviation
    Assume you have a dictionary and given a word, find whether its abbreviation is unique in the dictionary. A word's abbreviation is unique if no other word from the dictionary has the same abbreviation.

---
+++ Question288.java
@@ -0,0 +1,31 @@
+class ValidWordAbbr {
+    HashMap<String,String> map = new HashMap<String,String>();
+    public ValidWordAbbr(String[] dictionary) {
+        
+        for(int i=0; i<dictionary.length; i++){
+            if(dictionary[i].length() < 3)
+                continue;
+            String word = dictionary[i];
+            String key = word.charAt(0)+""+(word.length()-2)+word.charAt(word.length()-1);
+            if(!map.containsKey(key))
+                map.put(key,word);
+            else
+                map.put(key,"");
+        }
+    }
+    
+    public boolean isUnique(String word) {
+        if(word.equals(""))
+            return true;
+        String key = word.charAt(0)+""+(word.length()-2)+word.charAt(word.length()-1);
+        if(map.containsKey(key) && !map.containsValue(word))
+            return false;
+        return true;
+    }
+}
+
+/**
+ * Your ValidWordAbbr object will be instantiated and called as such:
+ * ValidWordAbbr obj = new ValidWordAbbr(dictionary);
+ * boolean param_1 = obj.isUnique(word);
+ */
